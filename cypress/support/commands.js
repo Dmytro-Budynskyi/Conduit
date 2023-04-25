da2cefeb3664d281c671166d3dd7dde7443e2645
Dmytro Budynskyi <115696726+Dmytro-Budynskyi@users.noreply.github.com>
2023-04-25 16:02:20 +0200

    Initialize reference_solution

---
+++ cypress/support/commands.js
@@ -0,0 +1,86 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+addMatchImageSnapshotCommand();
+
+Cypress.Commands.add('getByDataCy', (selector) => {
+  cy.get(`[data-cy="${selector}"]`);
+});
+
+Cypress.Commands.add('register', (email, username, password) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:1667/users',
+    body: {
+      email,
+      username,
+      password
+    }
+  });
+});
+
+Cypress.Commands.add('login', (email, username, password) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:1667/users',
+    body: {
+      email,
+      username,
+      password
+    }
+  }).then(response => {
+    cy.setCookie('drash_sess', response.body.user.token);
+  });
+});
+
+Cypress.Commands.add('createArticle', (title, description, body) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:1667/users',
+    body: {
+      email: 'riot@qa.team',
+      password: '12345Qwert!',
+      username: 'riot'
+    }
+  }).then(response => {
+    cy.setCookie('drash_sess', response.body.user.token);
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:1667/articles',
+      body: {
+        article: {
+          title,
+          description,
+          body,
+          tags: "",
+          author_id: response.body.user.id
+        }
+      }
+    });
+  });
+});
